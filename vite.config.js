622a36bbb447d2691cea99411be40eb94b21668e
Rafael Barboza <rafael.barboza@gruporpn.com.br>
2026-01-06 09:42:17 -0300

    Atualziando os cards para ter um tamanho unico, e o caminho base que tem que ficar alterando no servidor

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/playbook/',
   server: {
     host: true, // permite acesso por localhost e pela rede
     port: 5173, // porta padrão, pode ajustar
